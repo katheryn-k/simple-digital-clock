c226a6dcb3034b0116ab41a2ab9cd664d83dcaca
katheryn-k <kirichenko.ekaterynaa@yandex.ru>
2021-01-14 21:42:45 +0300

    feat: add date feature

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const hourElement = document.querySelector('.time__hour');
 const minuteElement = document.querySelector('.time__minute');
 const secondElement = document.querySelector('.time__second');
 
-let date = new Date();
+let dateObj = new Date();
 
-let hour = date.getHours();
-let minute = date.getMinutes();
-let second = date.getSeconds();
+let hour = dateObj.getHours();
+let minute = dateObj.getMinutes();
+let second = dateObj.getSeconds();
 
 if(hour >= 0 && hour <= 9) hour = '0' + hour;
 if(minute >= 0 && minute <= 9) minute = '0' + minute;
@@ -17,3 +17,18 @@ if(second >= 0 && second <= 9) second = '0' + hour;
 hourElement.innerHTML = hour;
 minuteElement.innerHTML = minute;
 secondElement.innerHTML = second;
+
+const dateElement = document.querySelector('.date');
+const dayElement = document.querySelector('.day');
+
+let date = dateObj.getDate();
+let month = dateObj.getMonth();
+let year = dateObj.getFullYear();
+
+
+if(date >= 0 && date <= 9) date = '0' + date;
+if(month === 0) month += 1;
+if (month >= 1 && month <= 9) month = '0' + month;
+if(year >= 0 && year <= 9) year = '0' + year;
+
+dateElement.innerHTML = `${date}/${month}/${year}`;
